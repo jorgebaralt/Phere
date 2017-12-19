9e54b0c5eb58675442e9dda79c535906a537342d
jorgebaralt <jorgebaraltq@gmaik.com>
2017-12-19 01:12:02 -0500

    user reference added on database

--- app/src/main/java/com/example/jorgebaralt/playnext/login/RegisterPersonalActivity.java
+++ app/src/main/java/com/example/jorgebaralt/playnext/login/RegisterPersonalActivity.java
@@ -14,22 +14,29 @@ import android.widget.Toast;
 
 import com.example.jorgebaralt.playnext.MainActivityUser;
 import com.example.jorgebaralt.playnext.R;
+import com.example.jorgebaralt.playnext.User;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterPersonalActivity extends AppCompatActivity {
+
+    private final static String TAG = "RegisterPersonal";
+    private final String accountType = "personal";
 
     private EditText mEmail;
     private EditText mUsername;
     private EditText mPassword;
     private Button mCreateAccountBtn;
-    private final static String TAG = "RegisterPersonal";
-    private String accountType = "personal";
     private FirebaseAuth mAuth;
-    private String email,password;
+    private String email,password,username;
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +49,10 @@ public class RegisterPersonalActivity extends AppCompatActivity {
         mUsername = (EditText) findViewById(R.id.editTxt_username_personal);
         mPassword = (EditText) findViewById(R.id.editTxt_password_personal);
         mCreateAccountBtn = (Button) findViewById(R.id.btn_createAccount_personal);
-        mAuth = FirebaseAuth.getInstance();
 
+        //initialize firebase instances
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         mCreateAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +60,7 @@ public class RegisterPersonalActivity extends AppCompatActivity {
                 //TODO: check for empty email and password, do also a trim for blank spaces.
                 email = mEmail.getText().toString();
                 password = mPassword.getText().toString();
+                username = mUsername.getText().toString();
 
                 Log.d(TAG, "onClick:    " +
                         " email: " + mEmail +
@@ -63,9 +73,13 @@ public class RegisterPersonalActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                if(task.isSuccessful()){
                                    Log.d(TAG, "onComplete: Success creating user");
-                                   FirebaseUser user = mAuth.getCurrentUser();
+
+                                   //Add to database new user, with email, username and accountType
+                                   addUserReference();
+
                                    Intent mainIntent = new Intent(RegisterPersonalActivity.this, MainActivityUser.class);
                                    startActivity(mainIntent);
+
                                }else {
                                    Log.w(TAG, "onComplete: Error creating user ", task.getException());
                                    Toast.makeText(RegisterPersonalActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
@@ -73,10 +87,30 @@ public class RegisterPersonalActivity extends AppCompatActivity {
                             }
                         });
 
-                //TODO: Add to database new user, with username and accountType
+
+
 
             }
         });
+    }
+
+    private void addUserReference(){
+        User newUser = new User(email,username,accountType);
+        db.collection("users")
+                .add(newUser)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "database: user added with ID = " + documentReference.getId());
+                    }
+
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Error adding user...: " + e.getMessage());
+                    }
+                });
     }
 
 }
