2f04c3f78c53f8d97ba11c9931a4ac57eb74bc44
Andres <agarciav91@gmail.com>
2018-02-25 16:58:32 -0400

    Connecting to Firebase

--- Phere-Android/app/src/main/java/com/phereapp/phere/phereHandling/CreateNewPhereActivity.java
+++ Phere-Android/app/src/main/java/com/phereapp/phere/phereHandling/CreateNewPhereActivity.java
@@ -1,18 +1,23 @@
 package com.phereapp.phere.phereHandling;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.phereapp.phere.Phere;
 import com.phereapp.phere.R;
@@ -20,10 +25,15 @@ import com.phereapp.phere.User;
 
 public class CreateNewPhereActivity extends AppCompatActivity {
 
+    private final static String TAG = "CreateNewPhere";
+    private final String phereType = "privacy";
+
     private EditText mPhereName;
     private EditText mPhereLocation;
     private RadioGroup mPrivacy;
     private RadioButton mPrivacyChoosen;
+    private Button mDisplay;
+    private String choosenPrivacy;
     private Button mCreatePhereButton;
     private String phereName, phereLocation;
     private String pheresCollection = "pheres";
@@ -42,14 +52,35 @@ public class CreateNewPhereActivity extends AppCompatActivity {
 
         mPhereName = (EditText) findViewById(R.id.editTxt_phereName_createPhere);
         mPhereLocation = (EditText) findViewById(R.id.editTxt_location_createPhere);
-        mCreatePhereButton = (Button) findViewById(R.id.btn_ok);
+        mCreatePhereButton = (Button) findViewById(R.id.btn_ok_create_phere);
 
         db = FirebaseFirestore.getInstance();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-        phereName = mPhereName.getText().toString();
-        phereLocation = mPhereLocation.getText().toString();
+        mPrivacy = (RadioGroup) findViewById(R.id.radio_choose_createPhere);
 
-        }
+        mCreatePhereButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phereName = mPhereName.getText().toString();
+                phereLocation = mPhereLocation.getText().toString();
+
+                int selectedId = mPrivacy.getCheckedRadioButtonId();
+                mPrivacyChoosen = (RadioButton) findViewById(selectedId);
+                choosenPrivacy = mPrivacyChoosen.getText().toString();
+
+                addUserReference();
+            }
+        });
+
     }
+
+    public void addUserReference(){
+        Phere newPhere = new Phere(phereName, phereLocation, choosenPrivacy);
+        db.collection(pheresCollection).document(phereName).set(newPhere);
+    }
+
+
+}
 
 
