00bd77e064fb9716d6ef95f00a8aedb4fcad687a
Andres <agarciav91@gmail.com>
2018-02-26 20:36:43 -0400

    Added Facebook login Feature

--- Phere-Android/app/src/main/java/com/phereapp/phere/login/StartActivity.java
+++ Phere-Android/app/src/main/java/com/phereapp/phere/login/StartActivity.java
@@ -3,6 +3,7 @@ package com.phereapp.phere.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,7 +11,24 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseUser;
+import com.phereapp.phere.MainActivityUser;
 import com.phereapp.phere.R;
 
 
@@ -18,6 +36,9 @@ import com.phereapp.phere.R;
 public class StartActivity extends AppCompatActivity {
     private Button mSignupBtn;
     private TextView mSigninBtn;
+    private FirebaseAuth mAuth;
+    private CallbackManager mCallbackManager;
+    private static final String EMAIL = "email";
     final private static String TAG = "StartActivity";
 
     @Override
@@ -27,6 +48,8 @@ public class StartActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mSignupBtn = (Button) findViewById(R.id.btn_email_start);
         mSigninBtn = (TextView) findViewById(R.id.btn_signin_start);
 
@@ -35,7 +58,7 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: Register Personal Account clicked");
-                Intent registerIntent = new Intent(StartActivity.this,SelectActivity.class);
+                Intent registerIntent = new Intent(StartActivity.this, SelectActivity.class);
                 startActivity(registerIntent);
             }
         });
@@ -45,10 +68,66 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: Sign in clicked");
-                Intent signinIntent = new Intent(StartActivity.this,SigninActivity.class);
+                Intent signinIntent = new Intent(StartActivity.this, SigninActivity.class);
                 startActivity(signinIntent);
             }
         });
 
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_button_facebook);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void handleFacebookAccessToken(AccessToken token){
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    // Sign in success, Go to the next Activity (MainActivityUser)
+                    Log.d(TAG, "signInWithCredential:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent loggedInIntent = new Intent(StartActivity.this, MainActivityUser.class);
+                    startActivity(loggedInIntent);
+                }
+
+                else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.getException());
+                    Toast.makeText(StartActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
