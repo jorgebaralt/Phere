0a75f8c8bd1b8b671155774ea58046a13df1fb9f
jorgebaralt <jorgebaraltq@gmail.com>
2018-03-05 17:27:03 -0500

    ready to retrieve joined pheres

---
+++ Phere-Android/app/src/main/java/com/phereapp/phere/selected_phere/SelectedPhereMainActivity.java
@@ -0,0 +1,15 @@
+package com.phereapp.phere.selected_phere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.phereapp.phere.R;
+
+public class SelectedPhereMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_phere_main);
+    }
+}
