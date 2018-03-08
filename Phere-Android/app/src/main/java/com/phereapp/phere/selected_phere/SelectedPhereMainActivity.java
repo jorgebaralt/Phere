15db7f13bc148e19931508a0d2eb3d1bb0f9e94a
Andres <agarciav91@gmail.com>
2018-03-07 21:40:03 -0400

    SelectedPhereActivity Changes

--- Phere-Android/app/src/main/java/com/phereapp/phere/selected_phere/SelectedPhereMainActivity.java
+++ Phere-Android/app/src/main/java/com/phereapp/phere/selected_phere/SelectedPhereMainActivity.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.phereapp.phere.R;
 import com.phereapp.phere.pojo.Phere;
 
 public class SelectedPhereMainActivity extends AppCompatActivity {
 
     private static String TAG = "SelectedPhereMainActivity";
+    private TextView mPhereDescription;
+    private ImageView mPhereProfilePicture;
     CollapsingToolbarLayout mPhereImage;
+    //Firebase
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +31,17 @@ public class SelectedPhereMainActivity extends AppCompatActivity {
             Log.d(TAG, "onCreate: WE GOT THE PHERE ************" + selectedPhere.getPhereName());
         }
 
+        //Firebase Storage
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        //Initialize the Views
+        mPhereDescription = (TextView) findViewById(R.id.txt_phere_description_selectedPhere);
+        mPhereDescription.setText(selectedPhere.getPhereDescription());
         mPhereImage = findViewById(R.id.toolbar_selectedPhere);
         mPhereImage.setTitle(selectedPhere.getPhereName());
+        mPhereProfilePicture = (ImageView) findViewById(R.id.img_phere_profilePicture_selectedPhere);
+
         //mPhereImage.setBackground();
         assert selectedPhere != null;
 
