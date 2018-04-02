bbff3de5ee465d3bd23764d769f0dbe8b43b18d1
Andres <agarciav91@gmail.com>
2018-04-01 22:46:06 -0400

    Added textviews/buttons to SelectedPhere layout

--- Phere-Android/app/src/main/java/com/phereapp/phere/selected_phere/SelectedPhereMainActivity.java
+++ Phere-Android/app/src/main/java/com/phereapp/phere/selected_phere/SelectedPhereMainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -17,8 +21,9 @@ import com.phereapp.phere.pojo.Phere;
 public class SelectedPhereMainActivity extends AppCompatActivity {
 
     private static String TAG = "SelectedPhereMainActivity";
-    private TextView mPhereDescription;
+    private TextView mPhereDescription, mPhereDate, mPhereLocation, mPhereMembers, mPherePlaylist;
     private DynamicImageView mPhereProfilePicture;
+    private Button mToggleDescription;
     CollapsingToolbarLayout mTitle;
     private String mPhereImageUrl;
 
@@ -32,18 +37,49 @@ public class SelectedPhereMainActivity extends AppCompatActivity {
             Log.d(TAG, "onCreate: WE GOT THE PHERE ************" + selectedPhere.getPhereName());
         }
         //Initialize the Views
-        mPhereDescription = (TextView) findViewById(R.id.txt_phere_description_selectedPhere);
-        mPhereDescription.setText(selectedPhere.getPhereDescription());
+        mPhereDescription = findViewById(R.id.txt_phere_description_selectedPhere);
         mTitle = findViewById(R.id.toolbar_selectedPhere);
+        mPhereProfilePicture = findViewById(R.id.img_profilePicture_selectedPhere);
+        mPhereDate = findViewById(R.id.txt_btn_date_selectedPhere);
+        mPhereLocation = findViewById(R.id.txt_btn_location_selectedPhere);
+        mPhereMembers = findViewById(R.id.txt_btn_members_selectedPhere);
+        mPherePlaylist = findViewById(R.id.txt_btn_playlist_selectedPhere);
+        mToggleDescription = findViewById(R.id.btn_toggleDescription_selectedPhere);
+
+        //Sets the title of the Phere
         mTitle.setTitle(selectedPhere.getDisplayPhereName());
-        mPhereProfilePicture = (DynamicImageView) findViewById(R.id.img_profilePicture_selectedPhere);
-        mPhereImageUrl = selectedPhere.getImageURL();
-
-
+        //Sets the description of the Phere
+        mPhereDescription.setText(selectedPhere.getPhereDescription());
         //Sets the profile image from the Download URL
+        mPhereImageUrl = selectedPhere.getImageURL();
         Glide.with(this).load(mPhereImageUrl).centerCrop().into(mPhereProfilePicture);
+        //Sets the date of the Phere
+        mPhereDate.setText(selectedPhere.getPhereDate());
+        //Sets the location of the phere
+        mPhereLocation.setText(selectedPhere.getPhereLocation());
 
+        //Makes the description text appear
+        mToggleDescription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Getting the current visibility of the TextView
+                int visibility = mPhereDescription.getVisibility();
+                //Getting the LayoutParameters to change the Height
+                ViewGroup.LayoutParams params = mPhereDescription.getLayoutParams();
+                if (visibility == View.INVISIBLE) {
+                    //Setting the Height of the TextView to Wrap_Content and showing the text
+                    params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                    mPhereDescription.setLayoutParams(params);
+                    mPhereDescription.setVisibility(View.VISIBLE);
+                } else {
+                    //Setting the Height of the TextView to 0dp and hiding the text
+                    params.height = 0;
+                    mPhereDescription.setLayoutParams(params);
+                    mPhereDescription.setVisibility(View.INVISIBLE);
+                }
 
+            }
+        });
 
     }
 }
